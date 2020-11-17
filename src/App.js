9c12af5959f13f8bf8cc29d889320aa198f5b8f9
Ronald <kyoberonald@gmail.com>
2020-11-18 00:16:44 +0300

    created project with create react app and added the styling with scss. I ran into a compatibility error which i solved with the help of https://stackoverflow.com/questions/64625050/error-node-sass-version-5-0-0-is-incompatible-with-4-0-0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react'; //jut inerted
 import './App.css';
+
+import HomePage from './homepage.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+      <HomePage />
+
     </div>
   );
 }
