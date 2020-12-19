332c4a35dd64ba2d486981aa7ed40b87c551138b
Ronald <kyoberonald@gmail.com>
2020-12-19 17:00:36 +0300

    styling the menu items+the hover effect+as well as fitting tthe image within the frame

--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -12,9 +12,11 @@ const MenuItem = ({ title, imageUrl, size }) => (
         }}
         className={`${size} menu-item`}
     >
-
+        <div className='background-image' style={{
+            backgroundImage: `url(${imageUrl})`
+        }}/>
         <div className = 'content'>
-            <h1 className = 'title'>{title}</h1>
+            <h1 className = 'title'>{title.toUpperCase()}</h1>
             <span className = 'subtitle'>SHOP NOW</span>
         </div>
     </div>
